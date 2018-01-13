26cb19126be50d791331c88a21820747eb536b79
Liubo <52511457@qq.com>
2018-01-13 22:24:34 +0800

    1.更新

---
+++ app/src/main/java/com/example/liubo/world_of_movie/Exception/ParamsErrorException.java
@@ -0,0 +1,17 @@
+package com.example.liubo.world_of_movie.Exception;
+
+/**
+ * Created by Liubo on 2018/1/13.
+ */
+
+public class ParamsErrorException extends RuntimeException {
+
+    public ParamsErrorException() {
+        super();
+    }
+
+    public ParamsErrorException(String s) {
+        super(s);
+    }
+
+}
--- app/src/main/java/com/example/liubo/world_of_movie/View/FragmentMe.java
+++ app/src/main/java/com/example/liubo/world_of_movie/View/FragmentMe.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import com.example.liubo.world_of_movie.BaseView.MyScrollView;
 import com.example.liubo.world_of_movie.R;
 
 /**
@@ -13,10 +14,29 @@ import com.example.liubo.world_of_movie.R;
  */
 
 public class FragmentMe extends Fragment {
+    private View headerView;
+    private ImageView imgView;
+    private View view;
+    private MyScrollView scrollview;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_me,null);
-        
+        view = inflater.inflate(R.layout.fragment_me,container,false);
+        init();
         return view;
     }
+
+    private void init() {
+        scrollview = (MyScrollView)view.findViewById(R.id.scrollview);
+        headerView = view.findViewById(R.id.lay_header);
+        imgView = (ImageView) view.findViewById(R.id.civ_avatar);
+
+        headerView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollview.setHeaderView(headerView, imgView);
+            }
+        });
+    }
+
 }
