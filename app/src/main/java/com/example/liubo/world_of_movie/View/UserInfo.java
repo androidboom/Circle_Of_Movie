d51de67d65499f147a1debee35d3599443c39116
Liubo <52511457@qq.com>
2018-01-21 16:18:27 +0800

    添加个人信息界面

---
+++ app/src/main/java/com/example/liubo/world_of_movie/View/UserInfo.java
@@ -0,0 +1,19 @@
+package com.example.liubo.world_of_movie.View;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.liubo.world_of_movie.R;
+
+/**
+ * Created by Liubo on 2018/1/21.
+ */
+
+public class UserInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userinfo);
+    }
+}
