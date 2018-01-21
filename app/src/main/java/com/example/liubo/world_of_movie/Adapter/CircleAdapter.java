866ca4d7a6547d15f143bec18dbcbec399140129
Liubo <52511457@qq.com>
2018-01-21 16:11:15 +0800

    1.更新资源文件1

--- app/src/main/java/com/example/liubo/world_of_movie/Adapter/CircleAdapter.java
+++ app/src/main/java/com/example/liubo/world_of_movie/Adapter/CircleAdapter.java
@@ -1,63 +1,79 @@
 package com.example.liubo.world_of_movie.Adapter;
 
+import android.content.Context;
 import android.database.DataSetObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.liubo.world_of_movie.R;
+
+import org.json.JSONArray;
 
 /**
  * Created by Liubo on 2018/1/21.
  */
 
-public class CircleAdapter implements Adapter {
+public class CircleAdapter extends BaseAdapter {
+    private JSONArray getData;
+    private Context context;
+    private LayoutInflater inflater;
+    private Holder holder;
 
-    @Override
-    public void registerDataSetObserver(DataSetObserver observer) {
-
-    }
-
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver observer) {
-
+    public CircleAdapter(Context context,JSONArray getData) {
+        this.getData = getData;
+        this.context = context;
+        this.inflater = (LayoutInflater) this.context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return getData.length();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return getData.optJSONObject(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.circleadapter_item, null);
+            holder = new Holder();
+            holder.user_icon = (ImageView) convertView.findViewById(R.id.user_icon);
+            holder.name = (TextView) convertView.findViewById(R.id.name);
+            holder.data = (TextView) convertView.findViewById(R.id.data);
+            holder.text = (TextView) convertView.findViewById(R.id.text);
+            holder.gridView = (GridView) convertView.findViewById(R.id.grid);
+            holder.pinglun = (ImageView)convertView.findViewById(R.id.iv_pinglun);
+            holder.praise = (ImageView)convertView.findViewById(R.id.iv_praise);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+        return convertView;
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        return 0;
+    public class Holder {
+        ImageView user_icon;
+        TextView name;
+        TextView data;
+        TextView text;
+        GridView gridView;
+        ImageView pinglun;
+        ImageView praise;
     }
 
-    @Override
-    public int getViewTypeCount() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
 }
--- app/src/main/java/com/example/liubo/world_of_movie/View/FragmentCircle.java
+++ app/src/main/java/com/example/liubo/world_of_movie/View/FragmentCircle.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.liubo.world_of_movie.Adapter.CircleAdapter;
 import com.example.liubo.world_of_movie.R;
 
+import org.json.JSONArray;
 import org.w3c.dom.Text;
 
 /**
@@ -16,11 +20,21 @@ import org.w3c.dom.Text;
  */
 
 public class FragmentCircle extends Fragment {
+    private View view;
+    private ListView lv;
+    private JSONArray Data;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_circle,null);
-
+        view = inflater.inflate(R.layout.fragment_circle,null);
+        init();
         return view;
     }
+
+    public void init(){
+        lv = (ListView)view.findViewById(R.id.lv);
+        CircleAdapter circleAdapter = new CircleAdapter(getActivity(),Data);
+        lv.setAdapter(circleAdapter);
+    }
+
 }
